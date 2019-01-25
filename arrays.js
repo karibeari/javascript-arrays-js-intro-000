d66153e4aa691c1a37c7298befff5acc8c41762b
Kari Ferenczy <klferenczy@gmail.com>
2019-01-25 23:32:33 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(chocolateBars, bar){
+  chocolateBars.unshift(bar);
+  return chocolateBars;
+}
+
+function destructivelyAddElementToEndOfArray(addElementToBeginningOfArray, ){
+  chocolateBars.unshift(bar);
+  return chocolateBars;
+}
