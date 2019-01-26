f57ba72e3eda7bad52505e856bd64c2483337c02
Kari Ferenczy <klferenczy@gmail.com>
2019-01-26 00:15:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,3 +33,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return newArray = array.slice(1);
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
