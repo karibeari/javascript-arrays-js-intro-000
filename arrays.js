8dac9b97b94d9acdfc9afb8e1bbac3f26be2b851
Kari Ferenczy <klferenczy@gmail.com>
2019-01-26 00:21:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, newElement){
 }
 
 function addElementToEndOfArray(array, element){
-  array.push(element);
+  var newArray = [array,...element]
   return newArray;
 }
 
