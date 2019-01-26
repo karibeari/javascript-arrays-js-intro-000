e476cfec7a96612c7f0274fa74382409634382ef
Kari Ferenczy <klferenczy@gmail.com>
2019-01-26 00:13:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,5 +31,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  return newArray = array.slice(0);
+  return newArray = array.slice(1);
 }
